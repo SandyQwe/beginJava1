b9102a0d8f15ee0a34638e12d80236144fa9e2bd
Alexander Sychev <sandyqwe@yandex.ru>
2016-08-10 01:23:18 +0500

    сделано задание 4 на булеву логику

--- src/geekbrains/java1/lesson1/homework/HomeWork.java
+++ src/geekbrains/java1/lesson1/homework/HomeWork.java
@@ -17,6 +17,7 @@ public class HomeWork {
         boolean b = true;
 
         //вывод переменных
+        System.out.println("Вывод объявленных переменных:");
         System.out.println("varByte = " + varByte);
         System.out.println("varShort = " + varShort);
         System.out.println("varInt = " + a);
@@ -34,13 +35,31 @@ public class HomeWork {
         float c1 = 16f;
         float d1 = 28f;
         firstMethodResult = firstMethod(a1, b1, c1, d1);
-        System.out.println("Результат операции над числами " + a1 + "*(" + b1 + "+(" + c1 + "/" + d1 + ")) составит " + firstMethodResult);
+        System.out.println("\n\nРезультат операции над числами " + a1 + "*(" + b1 + "+(" + c1 + "/" + d1 + ")) составит " + firstMethodResult);
+
+        // Написать метод, принимающий на вход два числа,
+        // и проверяющий что их сумма лежит в пределах 10 до 20,
+        // если да – вернуть true, в противном случае – false.
+        byte a2 = 8;
+        byte b2 = 10;
+        boolean bool2 = isItTrue(a2, b2);
+        System.out.print("\n\nПроверка, лежит ли сумма чисел "+a2 +" и "+b2+" в пределах от 10 до 20 включительно: ");
+        if (bool2) {
+            System.out.println("ЛЕЖИТ");
+        }
+        else {
+            System.out.println("НЕ ЛЕЖИТ");
+        }
 
     }
 
 
     private float firstMethod(float a, float b, float c, float d) {
         return a * (b + (c / d));
+    }
+
+    private boolean isItTrue(byte a, byte b) {
+        return ((a + b) >= 10 && (a + b) <= 20);
     }
 
 }
