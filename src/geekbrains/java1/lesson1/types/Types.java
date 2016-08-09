c5844536c2df55567a98135f2a6c1f3f64968105
Alexander Sychev <sandyqwe@yandex.ru>
2016-08-09 07:03:36 +0500

    345

---
+++ src/geekbrains/java1/lesson1/types/Types.java
@@ -0,0 +1,28 @@
+package geekbrains.java1.lesson1.types;
+
+/**
+ * Created by sadmin on 08.08.2016.
+ */
+public class Types {
+    public static void main(String[] args) {
+        byte varByte = 120;
+        short varShort = -32000;
+        int a = 200000000;
+        long l = 9000000000L;
+        float varFloat = 0.5f;
+        double varDouble = 0.5;
+        char c = 'f';
+        boolean b = true;
+
+        System.out.println("varByte = " + varByte);
+        System.out.println("varShort = " + varShort);
+        System.out.println("varInt = " + a);
+        System.out.println("varLong = " + l);
+        System.out.println("varFloat = " + varFloat);
+        System.out.println("varDouble = " + varDouble);
+        System.out.println("char = " + c);
+        System.out.println("varBoolean = " + b);
+    }
+
+
+}
