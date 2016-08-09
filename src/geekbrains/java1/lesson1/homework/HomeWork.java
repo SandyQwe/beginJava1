d6144ecb025b61c8f84acd4df4bb4108107e5f4a
Alexander Sychev <sandyqwe@yandex.ru>
2016-08-10 01:14:21 +0500

    косметические доделки

--- src/geekbrains/java1/lesson1/homework/HomeWork.java
+++ src/geekbrains/java1/lesson1/homework/HomeWork.java
@@ -5,15 +5,29 @@ package geekbrains.java1.lesson1.homework;
  */
 public class HomeWork {
     public void doIt() {
+
+        //объявление переменных
         byte varByte = 120;
         short varShort = -32000;
-        int r = 200000000;
+        int a = 200000000;
         long l = 9000000000L;
         float varFloat = 0.5f;
         double varDouble = 0.5;
         char c = 'f';
         boolean b = true;
 
+        //вывод переменных
+        System.out.println("varByte = " + varByte);
+        System.out.println("varShort = " + varShort);
+        System.out.println("varInt = " + a);
+        System.out.println("varLong = " + l);
+        System.out.println("varFloat = " + varFloat);
+        System.out.println("varDouble = " + varDouble);
+        System.out.println("char = " + c);
+        System.out.println("varBoolean = " + b);
+
+        //Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+        // где a,b,c,d – входные параметры этого метода.
         float firstMethodResult;
         float a1 = 10f;
         float b1 = 13f;
