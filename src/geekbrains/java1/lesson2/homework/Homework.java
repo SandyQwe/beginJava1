6f1aea8d33b9ad86286ec28c756c556ba5cf536d
Alexander Sychev <sandyqwe@yandex.ru>
2016-08-17 18:41:09 +0500

    закончил задание 1

--- src/geekbrains/java1/lesson2/homework/Homework.java
+++ src/geekbrains/java1/lesson2/homework/Homework.java
@@ -1,11 +1,25 @@
 package geekbrains.java1.lesson2.homework;
 
+import java.util.Random;
+
 /** dd
  * Created by sadmin on 17.08.2016.
  */
 class Homework {
     void doIt (){
+
+        // Создать массив состоящий из элементов 0 и 1, например int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
         int[] arr = new int[10];
+        Random rnd = new Random();
+        System.out.println("массив состоящий из элементов 0 и 1");
+        for (int i = 0; i < arr.length; i++) {
+            if (rnd.nextBoolean()) arr[i] = 1;
+            else arr[i] = 0;
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+
 
     }
 }
