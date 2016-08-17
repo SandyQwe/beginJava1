7fb630221bf1e862abe8615f6ca9bd8741a87257
Alexander Sychev <sandyqwe@yandex.ru>
2016-08-17 18:44:15 +0500

    закончил задание 2

--- src/geekbrains/java1/lesson2/homework/Homework.java
+++ src/geekbrains/java1/lesson2/homework/Homework.java
@@ -2,11 +2,12 @@ package geekbrains.java1.lesson2.homework;
 
 import java.util.Random;
 
-/** dd
+/**
+ * dd
  * Created by sadmin on 17.08.2016.
  */
 class Homework {
-    void doIt (){
+    void doIt() {
 
         // Создать массив состоящий из элементов 0 и 1, например int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
         int[] arr = new int[10];
@@ -15,10 +16,18 @@ class Homework {
         for (int i = 0; i < arr.length; i++) {
             if (rnd.nextBoolean()) arr[i] = 1;
             else arr[i] = 0;
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
 
+        //В массиве из предыдущего пункта, с помощью цикла заменить 0 на 1, 1 на 0;
+        System.out.println("инвертированный массив");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) arr[i]--;
+            else arr[i]++;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
 
     }
