86726a42ea406d120d44a9dec842fb90833e0a08
Alexander Sychev <sandyqwe@yandex.ru>
2016-08-22 00:00:28 +0500

    вроде сделал 4 задание

--- src/geekbrains/java1/lesson3/homework/NoughtsAndCrosses.java
+++ src/geekbrains/java1/lesson3/homework/NoughtsAndCrosses.java
@@ -91,28 +91,44 @@ class NoughtsAndCrosses {
         int y = lastCell[1];
         //проверка вертикалей
         for (int i = y; i >= 0; i--)
-            if (field[i][x] == c) if (++winLine == line) return true; else; else break;
+            if (field[i][x] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         for (int i = y + 1; i < fieldSizeY; i++)
-            if (field[i][x] == c) if (++winLine == line) return true; else; else break;
+            if (field[i][x] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         //проверка горизонталей
         winLine = 0;
         for (int i = x; i >= 0; i--)
-            if (field[y][i] == c) if (++winLine == line) return true; else; else break;
+            if (field[y][i] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         for (int i = x + 1; i < fieldSizeX; i++)
-            if (field[y][i] == c) if (++winLine == line) return true; else; else break;
+            if (field[y][i] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         //проверка диагонали слева сверху вправо вниз
         winLine = 0;
         for (int i = 0; i <= (x <= y ? x : y); i++)
-            if (field[y-i][x-i] == c) if (++winLine == line) return true; else; else break;
+            if (field[y - i][x - i] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         for (int i = 1; i < (fieldSizeX - x < fieldSizeY - y ? fieldSizeX - x : fieldSizeY - y); i++)
-            if (field[y+i][x+i] == c) if (++winLine == line) return true; else; else break;
+            if (field[y + i][x + i] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         //проверка диагонали слева снизу вправо вверх
         winLine = 0;
         for (int i = 0; i <= (fieldSizeY - y - 1 < x ? fieldSizeY - y - 1 : x); i++)
-            if (field[y+i][x-i] == c) if (++winLine == line) return true; else; else break;
+            if (field[y + i][x - i] == c) if (++winLine == line) return true;
+            else ;
+            else break;
         if (y != 0 && x != fieldSizeX - 1)
             for (int i = 1; i <= (y < fieldSizeX - x - 1 ? y : fieldSizeX - x - 1); i++)
-                if (field[y-i][x+i] == c) if (++winLine == line) return true; else; else break;
+                if (field[y - i][x + i] == c) if (++winLine == line) return true;
+                else ;
+                else break;
         return false;
     }
 
@@ -130,23 +146,74 @@ class NoughtsAndCrosses {
 
     private void aiTurn() {
         int x, y;
-        if (aiCanBlock(lastCell[0], lastCell[1])) aiBlock();
-        else
+        if (!aiCanBlock(lastCell[0], lastCell[1])) {
+            x = lastCell[0];
+            y = lastCell[1];
+        } else {
             do {
-                  x = rnd.nextInt(fieldSizeX);
-                  y = rnd.nextInt(fieldSizeY);
+                x = rnd.nextInt(fieldSizeX);
+                y = rnd.nextInt(fieldSizeY);
             } while (!emptyCell(x, y));
-        lastCell[0] = x;
-        lastCell[1] = y;
+            lastCell[0] = x;
+            lastCell[1] = y;
+        }
         field[y][x] = AI_DOT;
     }
 
-    private void aiBlock() {
-        // TODO: 21.08.2016 допилить
-    }
+//    private void aiBlock() {
+//        // TODO: 21.08.2016 допилить
+//    }
 
     private boolean aiCanBlock(int x, int y) {
-
+        //проверка на возможность блока справа
+        if (x > 0 && field[y][x - 1] == HUMAN_DOT && validCell(x + 1, y) && emptyCell(x + 1, y)) {
+            lastCell[0] = x + 1;
+            lastCell[1] = y;
+            return true;
+        }
+        //проверка на возможность блока слева
+        if (x < fieldSizeX - 1 && field[y][x + 1] == HUMAN_DOT && validCell(x - 1, y) && emptyCell(x - 1, y)) {
+            lastCell[0] = x - 1;
+            lastCell[1] = y;
+            return true;
+        }
+        //проверка на возможность блока снизу
+        if (y > 0 && field[y - 1][x] == HUMAN_DOT && validCell(x, y + 1) && emptyCell(x, y + 1)) {
+            lastCell[0] = x;
+            lastCell[1] = y + 1;
+            return true;
+        }
+        //проверка на возможность блока сверху
+        if (y < fieldSizeY - 1 && field[y + 1][x] == HUMAN_DOT && validCell(x, y - 1) && emptyCell(x, y - 1)) {
+            lastCell[0] = x;
+            lastCell[1] = y - 1;
+            return true;
+        }
+        //проверка на возможность блока снизу справа
+        if (x > 0 && y > 0 && field[y - 1][x - 1] == HUMAN_DOT && validCell(x + 1, y + 1) && emptyCell(x + 1, y + 1)) {
+            lastCell[0] = x + 1;
+            lastCell[1] = y + 1;
+            return true;
+        }
+        //проверка на возможность блока снизу слева
+        if (y > 0 && x < fieldSizeX - 1 && field[y - 1][x + 1] == HUMAN_DOT && validCell(x - 1, y + 1) && emptyCell(x - 1, y + 1)) {
+            lastCell[0] = x - 1;
+            lastCell[1] = y + 1;
+            return true;
+        }
+        //проверка на возможность блока сверху справа
+        if (x > 0 && y < fieldSizeY - 1 && field[y + 1][x - 1] == HUMAN_DOT && validCell(x + 1, y - 1) && emptyCell(x + 1, y - 1)) {
+            lastCell[0] = x + 1;
+            lastCell[1] = y - 1;
+            return true;
+        }
+        //проверка на возможность блока сверху слева
+        if (x < fieldSizeX - 1 && y < fieldSizeY - 1 && field[y + 1][x + 1] == HUMAN_DOT && validCell(x - 1, y - 1) && emptyCell(x - 1, y - 1)) {
+            lastCell[0] = x - 1;
+            lastCell[1] = y - 1;
+            return true;
+        }
+        //если нигде нет сдвоенных знаков, то будем пока считать, что блокировать нельзя
         return false;
     }
 
