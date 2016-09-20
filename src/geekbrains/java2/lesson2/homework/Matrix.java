b2dc440f5d82d15ec3aab54f0160f7a10bf976a9
Alexander Sychev <sandyqwe@yandex.ru>
2016-09-20 17:47:20 +0500

    Начал делать домашнее задание к третьему уроку

--- src/geekbrains/java2/lesson2/homework/Matrix.java
+++ src/geekbrains/java2/lesson2/homework/Matrix.java
@@ -1,12 +1,12 @@
 package geekbrains.java2.lesson2.homework;
 
- class Matrix {
+class Matrix {
     private String[][] matrix;
     private int[][] numMatrix;
 
-     Matrix(String line) throws IllegalMatrixException {
-            this.matrix = SetMatrix(line);
-            this.numMatrix = MatrixToInt(matrix);
+    Matrix(String line) throws IllegalMatrixException {
+        this.matrix = SetMatrix(line);
+        this.numMatrix = MatrixToInt(matrix);
     }
 
     private String[][] SetMatrix(String line) throws IllegalMatrixException {
@@ -18,19 +18,20 @@ package geekbrains.java2.lesson2.homework;
         if (newLine.length > 4) throw new IllegalMatrixException("Слишком много строк (должно быть 4)");
         for (int i = 0; i < newLine.length; i++) {
             String[] newElement = newLine[i].split(" ");
-            if (newElement.length != 4) throw new IllegalMatrixException("Неправильное количество столбцов (должно быть 4)");
+            if (newElement.length != 4)
+                throw new IllegalMatrixException("Неправильное количество столбцов (должно быть 4)");
             newMatrix[i] = newElement;
         }
         return newMatrix;
     }
 
-    private int[][] MatrixToInt (String[][] matrix) throws IllegalMatrixException {
+    private int[][] MatrixToInt(String[][] matrix) throws IllegalMatrixException {
         int[][] intMatrix = new int[4][4];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                try{
+                try {
                     intMatrix[i][j] = Integer.parseInt(matrix[i][j]);
-                }catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
                     throw new IllegalMatrixException("Один из элементов введённой матрицы не является числом");
                 }
             }
@@ -38,7 +39,7 @@ package geekbrains.java2.lesson2.homework;
         return intMatrix;
     }
 
-     float MatrixOperation(){
+    float MatrixOperation() {
         float result = 0.0f;
         for (int i = 0; i < this.numMatrix.length; i++) {
             for (int j = 0; j < this.numMatrix[i].length; j++) result += this.numMatrix[i][j];
@@ -46,17 +47,15 @@ package geekbrains.java2.lesson2.homework;
         return result /= 2;
     }
 
-     void ViewMatrix () {
+    void ViewMatrix() {
         System.out.println("Ваша матрица:");
         for (int i = 0; i < numMatrix.length; i++) {
             for (int j = 0; j < numMatrix[i].length; j++) {
-                System.out.print(numMatrix[i][j]+" ");
+                System.out.print(numMatrix[i][j] + " ");
             }
             System.out.println();
         }
     }
-
-
 
 
 }
