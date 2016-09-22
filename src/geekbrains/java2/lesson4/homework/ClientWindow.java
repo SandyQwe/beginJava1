ad2a892e3ce937ec183d501f081c64302a63c09c
Alexander Sychev <sandyqwe@yandex.ru>
2016-09-22 18:57:59 +0500

    сделал оставшиеся основные элементы чта - основное окно с текстом, поле ввода текста пользователем, кнопка отправки текста

--- src/geekbrains/java2/lesson4/homework/ClientWindow.java
+++ src/geekbrains/java2/lesson4/homework/ClientWindow.java
@@ -1,9 +1,11 @@
 package geekbrains.java2.lesson4.homework;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class ClientWindow extends JFrame {
 
@@ -16,27 +18,68 @@ public class ClientWindow extends JFrame {
         //Основное меню - на будущее
         JMenuBar mainMenu = new JMenuBar();
         JMenu mainMenuFirst = new JMenu("Main");
-        mainMenuFirst.setMnemonic(0);
-        mainMenuFirst.add(new JMenuItem("Register..."), 0);
-        mainMenuFirst.add(new JMenuItem("Login..."), 0);
-        mainMenuFirst.add(new JMenuItem("Change password..."), 0);
-        mainMenuFirst.add(new JMenuItem("Logout"), 1);
-        mainMenuFirst.add(new JMenuItem("Exit", 2)).addActionListener(new ActionListener() {
+        mainMenuFirst.setMnemonic(KeyEvent.VK_M);
+        mainMenuFirst.add(new JMenuItem("Register...", KeyEvent.VK_R));
+        mainMenuFirst.add(new JMenuItem("Login...", KeyEvent.VK_L));
+        mainMenuFirst.add(new JMenuItem("Change password...", KeyEvent.VK_N));
+        mainMenuFirst.add(new JMenuItem("Logout", KeyEvent.VK_O));
+        mainMenuFirst.add(new JMenuItem("Exit", KeyEvent.VK_X)).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
         JMenu mainMenuSecond = new JMenu("Chat");
-        mainMenuSecond.setMnemonic(0);
-        mainMenuSecond.add(new JMenuItem("Clear chat window"));
-        mainMenuSecond.add(new JMenuItem("Save chat to file..."));
+        mainMenuSecond.setMnemonic(KeyEvent.VK_C);
+        mainMenuSecond.add(new JMenuItem("Clear chat window", KeyEvent.VK_E));
+        mainMenuSecond.add(new JMenuItem("Save chat to file...", KeyEvent.VK_S));
         JMenu mainMenuLast = new JMenu("Help");
-        mainMenuLast.setMnemonic(0);
+        mainMenuLast.setMnemonic(KeyEvent.VK_F1);
         mainMenu.add(mainMenuFirst);
         mainMenu.add(mainMenuSecond);
         mainMenu.add(mainMenuLast);
         add(mainMenu, BorderLayout.NORTH);
+
+        //Основное окно чата, переписка
+        JTextArea mainChatText = new JTextArea();
+        mainChatText.setBorder(new LineBorder(Color.DARK_GRAY, 1));
+        mainChatText.setBackground(Color.WHITE);
+        mainChatText.setEditable(false);
+        JScrollPane mainChatScroll = new JScrollPane(mainChatText);
+        add(mainChatScroll, BorderLayout.CENTER);
+        mainChatText.setText("Text");
+
+        //Нижняя часть окна, область для набора текста и кнопка отправки
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
+        JTextField textField = new JTextField();
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textField.getText().equals("")) {
+                    mainChatText.append(textField.getText() + "\n");
+                    mainChatText.setCaretPosition(mainChatText.getDocument().getLength());
+                    textField.setText("");
+                    textField.requestFocus();
+                }
+            }
+        });
+        JButton sendTextButton = new JButton("Send");
+        sendTextButton.setMnemonic(KeyEvent.VK_ENTER);
+        sendTextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textField.getText().equals("")) {
+                    mainChatText.append(textField.getText() + "\n");
+                    mainChatText.setCaretPosition((mainChatText.getDocument().getLength()));
+                    textField.setText("");
+                    textField.requestFocus();
+                }
+            }
+        });
+        bottomPanel.add(textField, BorderLayout.CENTER);
+        bottomPanel.add(sendTextButton, BorderLayout.EAST);
+        add(bottomPanel, BorderLayout.SOUTH);
 
 
         setVisible(true);
