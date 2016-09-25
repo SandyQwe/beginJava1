6da29aa033af9cfa0fdee1772198f99befa586d3
Alexander Sychev <sandyqwe@yandex.ru>
2016-09-25 23:36:24 +0500

    примерно разобрался с парсингом информации о погоде с помощью библиотеки парсирга Jsoup

--- src/Diary/TestParseHTML.java
+++ src/Diary/TestParseHTML.java
@@ -5,20 +5,29 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class TestParseHTML {
     public static void main(String[] args) {
 
+        File input = new File("weather_sample.htm");
         Document html = null;
         try {
-            html = Jsoup.connect("http://www.pogodaiklimat.ru/weather.php?id=28440&bday=1&fday=31&amonth=8&ayear=2016").get();
+            html = Jsoup.parse(input, "UTF-8");
+//            html = Jsoup.connect("http://www.pogodaiklimat.ru/weather.php?id=28440&bday=1&fday=31&amonth=8&ayear=2016").get();
         } catch (IOException e) {
             e.printStackTrace();
         }
         Elements body = html.getElementsByAttributeValueContaining("height", "30");
-        System.out.println(body);
+
+        for (Element element:body) {
+            System.out.println(element.child(10).getElementsByTag("nobr").text().replaceAll("\\+",""));
+        }
+        ArrayList<WeatherEvent> weather = new ArrayList<>();
+        //System.out.println(body);
 
     }
 }
