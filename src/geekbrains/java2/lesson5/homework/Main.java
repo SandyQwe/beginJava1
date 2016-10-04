6bf6366f7281cf7f16c6b5f6ed71e2515c7d3eab
Alexander Sychev <sandyqwe@yandex.ru>
2016-10-04 13:48:45 +0500

    Почистил форматирование

--- src/geekbrains/java2/lesson5/homework/Main.java
+++ src/geekbrains/java2/lesson5/homework/Main.java
@@ -2,29 +2,25 @@ package geekbrains.java2.lesson5.homework;
 
 
 public class Main {
-    static final int size = 1000000;
-    static final int h = size / 2;
-
+    private static final int size = 1000000;
+    private static final int h = size / 2;
 
     public static void main(String[] args) {
         method1();
         method2();
-
-
     }
 
 
-    public static void method1() {
+    private static void method1() {
         float[] arr = new float[size];
         for (int i = 0; i < arr.length; i++) arr[i]++;
         long a = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++)
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         System.out.println("Первый метод " + (System.currentTimeMillis() - a));
-
     }
 
-    public static void method2() {
+    private static void method2() {
         float[] arr = new float[size];
         for (int i = 0; i < arr.length; i++) arr[i]++;
         long a = System.currentTimeMillis();
@@ -40,7 +36,6 @@ public class Main {
             }
         });
         t1.start();
-
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -49,18 +44,15 @@ public class Main {
             }
         });
         t2.start();
-
         try {
             t1.join();
             t2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h - 1, h);
         System.out.println("Второй метод " + (System.currentTimeMillis() - a));
-
     }
 
 }
