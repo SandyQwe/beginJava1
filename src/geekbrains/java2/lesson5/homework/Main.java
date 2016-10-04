df6ea883ed85da3a75fb592d5415cfd7bdef197e
Alexander Sychev <sandyqwe@yandex.ru>
2016-10-04 13:44:16 +0500

    Исправил недочёты и ошибки запуска потоков

--- src/geekbrains/java2/lesson5/homework/Main.java
+++ src/geekbrains/java2/lesson5/homework/Main.java
@@ -18,14 +18,8 @@ public class Main {
         float[] arr = new float[size];
         for (int i = 0; i < arr.length; i++) arr[i]++;
         long a = System.currentTimeMillis();
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
         System.out.println("Первый метод " + (System.currentTimeMillis() - a));
 
     }
@@ -38,31 +32,30 @@ public class Main {
         float[] a2 = new float[h];
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
-        new Thread(new Runnable() {
+        Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < a1.length; i++)
                     a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
-        }).run();
+        });
+        t1.start();
 
-        new Thread(new Runnable() {
+        Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < a2.length; i++)
                     a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
-        }).run();
+        });
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h - 1, h);
