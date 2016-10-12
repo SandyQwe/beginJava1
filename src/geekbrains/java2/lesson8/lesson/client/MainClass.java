1be53cd69d73ba449f1a2c176e4fa2f05ed77166
Alexander Sychev <sandyqwe@yandex.ru>
2016-10-12 07:43:44 +0500

    загрузил код с урока 8, почистил лишнюю доступность классов и методов, почистил лишние импорты

---
+++ src/geekbrains/java2/lesson8/lesson/client/MainClass.java
@@ -0,0 +1,7 @@
+package geekbrains.java2.lesson8.lesson.client;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MyWindow w = new MyWindow();
+    }
+}
---
+++ src/geekbrains/java2/lesson8/lesson/server/MainClass.java
@@ -0,0 +1,7 @@
+package geekbrains.java2.lesson8.lesson.server;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MyServer server = new MyServer();
+    }
+}
