104f6aa3fcde655730cefc44af97311e9fa32769
Alexander Sychev <sandyqwe@yandex.ru>
2016-11-14 21:54:09 +0500

    сделал третье задание к 3 уроку 3 части курса Java

--- src/geekbrains/java3/lesson3/homework/MainClass.java
+++ src/geekbrains/java3/lesson3/homework/MainClass.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,7 +33,7 @@ public class MainClass {
             }
         }
         for (int i = 0; i < byteArray.length - 1; i++) System.out.print(byteArray[i] + ", ");
-        System.out.println(byteArray[byteArray.length - 1] + ".");
+        System.out.println(byteArray[byteArray.length - 1] + ".\n\n\n");
 
 
 //        2) Последовательно сшить 10 файлов в один(файлы также ~100 байт).
@@ -62,7 +63,7 @@ public class MainClass {
         SequenceInputStream input2 = new SequenceInputStream(e);
         BufferedOutputStream outputFile = null;
         try {
-            outputFile = new BufferedOutputStream(new FileOutputStream(".\\output.txt"));
+            outputFile = new BufferedOutputStream(new FileOutputStream("output.txt"));
             int n = input2.read();
             while (n != -1) {
                 outputFile.write(n);
@@ -85,5 +86,43 @@ public class MainClass {
 // вводим страницу, программа выводит ее в консоль(за страницу можно принимаем 1800 символов). Время чтения файла
 // должно находится в разумных пределах(программа не должна загружаться дольше 10 секунд),
 // ну и чтение тоже не должно занимать >5 секунд.
+        final int PAGE_SYMBOLS = 1800;
+        DataInputStream input3 = null;
+        ArrayList<String> pagedText = new ArrayList<>();
+        try {
+            long t = System.currentTimeMillis();
+            input3 = new DataInputStream(new BufferedInputStream(new FileInputStream(".\\bigtext.txt"))); //тут лежит файл 11 мегабайт
+            StringBuffer s1 = new StringBuffer();
+            try {
+                while (true) {
+                    for (int i = 0; i < PAGE_SYMBOLS; i++) {
+                        s1.append((char) input3.readByte());
+                    }
+                    pagedText.add(s1.toString());
+                    s1 = new StringBuffer();
+                }
+            } catch (EOFException e2) {
+                pagedText.add(s1.toString());
+                System.out.println("Всего страниц: " + pagedText.size());
+                System.out.println("Время загрузки и текста и разбивки его на страницы: " + (System.currentTimeMillis() - t) + " миллисекунд");
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                input3.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        System.out.print("_______________________________________________________________\nВведите номер страницы для вывода на экран:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= pagedText.size() && n > 0)
+            System.out.println(pagedText.get(n - 1));
+        else
+            System.out.println("Вы ввели неправильный номер страницы");
+        sc.close();
+
     }
 }
